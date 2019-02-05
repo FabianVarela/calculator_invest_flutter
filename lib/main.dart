19ff675aee6e4e951bdb74c387b3313245744eaa
Fabián Varela Bonett <jfabian.303@gmail.com>
2019-02-05 13:14:07 -0500

    Adding text, select buttons styles to the form

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,153 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+void main() {
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Invest Calculator App', 
+      home: CalculateFrom(),
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.blueGrey,
+        accentColor: Colors.blueAccent,
+        brightness: Brightness.dark
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    )
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class CalculateFrom extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() {
+    return CalculateFormState();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class CalculateFormState extends State<CalculateFrom> {
+  var currencies = ['COP','USD','EUR','Other'];
+  final minimumPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
+
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+
     return Scaffold(
+      //resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Simple Invest Calculator')
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Container(
+        margin: EdgeInsets.all(minimumPadding * 2),
+        child: ListView(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            getAssetImage(),
+            Padding(
+              padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: textStyle,
+                decoration: InputDecoration(
+                  labelText: 'Principal value',
+                  hintText: 'Enter percent value',
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5)
+                  )
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Padding(
+              padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                style: textStyle,
+                decoration: InputDecoration(
+                  labelText: 'Rate of interest',
+                  hintText: 'Enter principal value, ej, 10000',
+                  labelStyle: textStyle,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5)
+                  )
+                ),
+              )
             ),
+            Padding(
+              padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      decoration: InputDecoration(
+                        labelText: 'Time',
+                        hintText: 'Time in years',
+                        labelStyle: textStyle,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)
+                        )
+                      ),
+                    )
+                  ),
+                  Container(width: minimumPadding * 5),
+                  Expanded(
+                    child: DropdownButton<String>(
+                      items: currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value)
+                        );
+                      }).toList(),
+                      value: currencies[0],
+                      onChanged: (String newValueSelected) {
+
+                      },
+                    )
+                  )
+                ],
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: minimumPadding, bottom: minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      child: Text('Calculate', textScaleFactor: 1.5),
+                      onPressed: () {
+
+                      }
+                    ),
+                  ),
+                  Container(width: minimumPadding *1),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Colors.grey,
+                      textColor: Colors.white,
+                      child: Text('Reset', textScaleFactor: 1.5),
+                      onPressed: () {
+                        
+                      }
+                    ),
+                  )
+                ],
+              )
+            ),
+            Padding(
+              padding: EdgeInsets.all(minimumPadding * 2),
+              child: Text('Todo text', style: textStyle)
+            )
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+        )
+      )
     );
+  }
+
+  Widget getAssetImage() {
+    AssetImage assetImage = AssetImage('images/invest.png');
+    Image image = Image(image: assetImage, width: 125, height: 125);
+
+    return Container(child: image, margin: EdgeInsets.all(minimumPadding * 10));
   }
 }
