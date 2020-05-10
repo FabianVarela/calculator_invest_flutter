676926988891ae56292279e2faeb4ab508464bfc
FabianVarela <jfabian.303@gmail.com>
2020-05-10 12:24:20 -0500

    Set app with portrait orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:calculator_invest_flutter/ui/calculate_form.ui.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations(
+      <DeviceOrientation>[DeviceOrientation.portraitUp]);
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
