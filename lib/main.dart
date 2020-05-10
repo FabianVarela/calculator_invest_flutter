2842fa109902160678f8cabc8578b83f52b2c984
FabianVarela <jfabian.303@gmail.com>
2020-05-10 11:42:27 -0500

    Update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:calculator_invest_flutter/ui/calculate_form.ui.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Simple Invest Calculator App',
       home: CalculateFrom(),
@@ -13,8 +21,8 @@ void main() {
         accentColor: Colors.blue,
         backgroundColor: Colors.white,
         iconTheme: IconThemeData(color: Colors.blueAccent),
-        errorColor: Colors.red
+        errorColor: Colors.red,
       ),
-    ),
-  );
+    );
+  }
 }
